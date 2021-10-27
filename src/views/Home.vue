1ebbfb0adebef97e1908501e564958d4552ef368
Daniele Tentoni <d.tentoni@wedoit.io>
2021-10-27 21:47:21 +0200

    Use env message.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld :msg="message" />
   </div>
 </template>
 
@@ -14,5 +14,10 @@ export default {
   components: {
     HelloWorld,
   },
+  data() {
+    return {
+      message=process.env.MESSAGE
+    }
+  }
 };
 </script>
