f9988c1c02051e27c97830585a85ddab60d162d7
Daniele Tentoni <d.tentoni@wedoit.io>
2021-10-27 23:30:00 +0200

    Use new env vars.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -107,6 +106,7 @@
         >
       </li>
     </ul>
+    <h1>MESSAGE: {{ msg }}</h1>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld :msg="message" />
+    <h1>COMPUTED: {{ computed_message }}</h1>
+    <h1>url: {{ url }}</h1>
   </div>
 </template>
 
@@ -14,10 +16,16 @@ export default {
   components: {
     HelloWorld,
   },
+  computed: {
+    computed_message: function () {
+      return process.env.VUE_APP_MESSAGE;
+    },
+  },
   data() {
     return {
-      message=process.env.MESSAGE
-    }
-  }
+      url: process.env.BASE_URL,
+      message: process.env.PUBLIC_PATH,
+    };
+  },
 };
 </script>
